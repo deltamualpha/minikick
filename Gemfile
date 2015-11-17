2231f6d5521e9e50946aabce510c142f956642b8
David Ashby <delta.mu.alpha@gmail.com>
2015-11-16 23:32:16 -0500

    add code coverage, re-add to_json test

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source "https://rubygems.org"
 gem 'luhn-ruby'
 gem 'json'
 gem 'rspec'
+gem 'simplecov', :require => false, :group => :test
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -22,6 +22,13 @@ RSpec.describe Project do
       expect{ project.add_backer 'John', 4111111111111111, 50 }.to raise_error
     end
 
+    it "sums all pledges on a project" do
+      project = Project.new('foobar', 1)
+      project.add_backer 'John', 4111111111111111, 50
+      project.add_backer 'Mark', 5555555555554444, 75
+      expect(project.pledge_total).to eq(125)
+    end
+
     it "returns all projects a backer has pledged to" do
       expect(Project.find_backer_projects("Mike").size).to eq(0)
       project = Project.new 'foobar1', 1
@@ -33,5 +40,9 @@ RSpec.describe Project do
       expect(Project.find_backer_projects("John").size).to eq(2)
     end
 
+    it "serializes to JSON" do
+      expect( Project.new('foo_bar', 1, [["john", 4111111111111111, 50]]).to_json )
+        .to eq('["foo_bar",1,[["john",4111111111111111,50]]]')
+    end
   end
 end
